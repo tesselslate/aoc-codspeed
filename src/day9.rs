4e32ee5b84a4cbd4010ca59bbd99b0e12a2981d9
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-12-10 03:30:16 -0500

    day 9 part 2 no opt
    it sucks but it works. i will go to bed

--- src/day9.rs
+++ src/day9.rs
@@ -36,95 +36,6 @@ impl Index<usize> for DiskA {
     }
 }
 
-struct FreeBuckets {
-    buckets: [[u16; LEN / 2]; 10],
-    read: [usize; 10],
-    write: [usize; 10],
-}
-
-impl Default for FreeBuckets {
-    fn default() -> Self {
-        Self {
-            buckets: [[0; LEN / 2]; 10],
-            read: [0; 10],
-            write: [0; 10],
-        }
-    }
-}
-
-impl FreeBuckets {
-    pub fn add(&mut self, sz: u8, pos: usize) {
-        let sz = sz as usize;
-
-        self.buckets[sz][self.write[sz]] = pos as u16;
-        self.write[sz] += 1;
-    }
-
-    pub fn get(&mut self, sz: u8) -> Option<u16> {
-        let sz = sz as usize;
-
-        if self.read[sz] == self.write[sz] {
-            None
-        } else {
-            let pos = self.buckets[sz][self.read[sz]];
-            self.read[sz] += 1;
-            Some(pos)
-        }
-    }
-
-    pub fn peek(&self, sz: u8) -> Option<u16> {
-        let sz = sz as usize;
-
-        if self.read[sz] == self.write[sz] {
-            None
-        } else {
-            Some(self.buckets[sz][self.read[sz]])
-        }
-    }
-
-    pub fn insert(&mut self, sz: u8, pos: u16) {
-        let sz = sz as usize;
-
-        // TODO: binary search? are the buckets big enough
-        self.buckets[sz][self.write[sz]] = pos;
-        self.write[sz] += 1;
-        self.buckets[sz][self.read[sz]..self.write[sz]].sort_unstable();
-    }
-}
-
-struct Files {
-    pos: [u16; LEN / 2 + 1],
-    sz: [u8; LEN / 2 + 1],
-    len: usize,
-}
-
-impl Default for Files {
-    fn default() -> Self {
-        Self {
-            pos: [0; LEN / 2 + 1],
-            sz: [0; LEN / 2 + 1],
-            len: 0,
-        }
-    }
-}
-
-impl Files {
-    pub fn get(&self, index: usize) -> (u16, u8) {
-        (self.pos[index], self.sz[index])
-    }
-
-    pub fn set(&mut self, index: usize, pos: u16, sz: u8) {
-        self.pos[index] = pos;
-        self.sz[index] = sz;
-    }
-
-    pub fn push(&mut self, pos: u16, sz: u8) {
-        self.pos[self.len] = pos;
-        self.sz[self.len] = sz;
-        self.len += 1;
-    }
-}
-
 fn inner_p1<const LEN: usize>(input: &[u8]) -> u64 {
     let input = &input[..LEN];
 
@@ -166,54 +77,51 @@ fn inner_p1<const LEN: usize>(input: &[u8]) -> u64 {
 fn inner_p2<const LEN: usize>(input: &[u8]) -> u64 {
     let input = &input[..LEN];
 
-    let mut free = FreeBuckets::default();
-    let mut files = Files::default();
+    let mut files = [(0u32, 0u8); 10000];
+    let mut free = [(0u32, 0u8); 10000];
 
     let mut pos = 0;
     for i in 0..LEN {
         if i % 2 == 0 {
-            files.push(pos as u16, input[i] & 0xF);
+            files[i / 2] = (pos, input[i] & 0xF);
         } else {
-            free.add(input[i] & 0xF, pos);
+            free[i / 2] = (pos, input[i] & 0xF);
         }
-
-        pos += (input[i] & 0xF) as usize;
+        pos += (input[i] & 0xF) as u32;
     }
 
-    for i in (0..files.len).rev() {
-        let (pos, sz) = files.get(i);
+    for i in (0..=LEN / 2).rev() {
+        let (pos, sz) = files[i];
 
-        let mut min_sz = 0;
-        let mut min_pos = u16::MAX;
-        for try_sz in sz..=9 {
-            if let Some(free_pos) = free.peek(try_sz) {
-                if free_pos < min_pos {
-                    min_pos = free_pos;
-                    min_sz = try_sz;
-                }
+        for j in 0..LEN / 2 {
+            let (free_pos, free_sz) = free[j];
+            if free_pos > pos {
+                break;
+            }
+
+            if free_sz == sz {
+                files[i] = (free_pos, sz);
+                free[j] = (free_pos, 0);
+                break;
+            } else if free_sz > sz {
+                files[i] = (free_pos, sz);
+                free[j] = (free_pos + sz as u32, free_sz - sz);
+                break;
             }
         }
-
-        if min_sz == 0 || min_pos > pos {
-            continue;
-        }
-
-        let free_pos = free.get(min_sz).unwrap();
-        files.set(i, free_pos, sz);
-        free.insert(sz, pos);
-        if min_sz > sz {
-            free.insert(min_sz - sz, free_pos + sz as u16);
-        }
     }
 
-    let mut sum = 0;
-    for i in 0..files.len {
-        let (pos, sz) = files.get(i);
-        for j in pos as u64..pos as u64 + sz as u64 {
-            sum += i as u64 * j;
-        }
-    }
-    sum
+    files
+        .iter()
+        .enumerate()
+        .map(|(id, &(pos, sz))| {
+            let mut x = 0;
+            for i in pos as u64..pos as u64 + sz as u64 {
+                x += id as u64 * i;
+            }
+            x
+        })
+        .sum()
 }
 
 pub fn part1(input: &str) -> u64 {
