e7976026062777b550657255c5e5c97443a2f3b0
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-12-10 03:13:41 -0500

    dya 9 part 2 buckets
    i want to go to bed

--- src/day9.rs
+++ src/day9.rs
@@ -36,48 +36,98 @@ impl Index<usize> for DiskA {
     }
 }
 
-struct DiskB {
-    file_id: [u16; LEN / 2 + 1],
-    file_pos: [u16; LEN / 2 + 1],
-    file_sz: [u8; LEN / 2 + 1],
-    file_len: usize,
-
-    free_pos: [u16; LEN / 2],
-    free_sz: [u8; LEN / 2],
-    free_len: usize,
+struct FreeBuckets {
+    buckets: [[u16; LEN / 2]; 10],
+    read: [usize; 10],
+    write: [usize; 10],
 }
 
-impl Default for DiskB {
+impl Default for FreeBuckets {
     fn default() -> Self {
         Self {
-            file_id: [0; LEN / 2 + 1],
-            file_pos: [0; LEN / 2 + 1],
-            file_sz: [0; LEN / 2 + 1],
-            file_len: 0,
-
-            free_pos: [0; LEN / 2],
-            free_sz: [0; LEN / 2],
-            free_len: 0,
+            buckets: [[0; LEN / 2]; 10],
+            read: [0; 10],
+            write: [0; 10],
         }
     }
 }
 
-impl DiskB {
-    pub fn push_file(&mut self, pos: usize, sz: u8, id: u16) {
-        self.file_id[self.file_len] = id;
-        self.file_pos[self.file_len] = pos as u16;
-        self.file_sz[self.file_len] = sz;
-        self.file_len += 1;
+impl FreeBuckets {
+    pub fn add(&mut self, sz: u8, pos: usize) {
+        let sz = sz as usize;
+
+        self.buckets[sz][self.write[sz]] = pos as u16;
+        self.write[sz] += 1;
     }
 
-    pub fn push_free(&mut self, pos: usize, sz: u8) {
-        self.free_pos[self.free_len] = pos as u16;
-        self.free_sz[self.free_len] = sz;
-        self.free_len += 1;
+    pub fn get(&mut self, sz: u8) -> Option<u16> {
+        let sz = sz as usize;
+
+        if self.read[sz] == self.write[sz] {
+            None
+        } else {
+            let pos = self.buckets[sz][self.read[sz]];
+            self.read[sz] += 1;
+            Some(pos)
+        }
+    }
+
+    pub fn peek(&self, sz: u8) -> Option<u16> {
+        let sz = sz as usize;
+
+        if self.read[sz] == self.write[sz] {
+            None
+        } else {
+            Some(self.buckets[sz][self.read[sz]])
+        }
+    }
+
+    pub fn insert(&mut self, sz: u8, pos: u16) {
+        let sz = sz as usize;
+
+        // TODO: binary search? are the buckets big enough
+        self.buckets[sz][self.write[sz]] = pos;
+        self.write[sz] += 1;
+        self.buckets[sz][self.read[sz]..self.write[sz]].sort_unstable();
+    }
+}
+
+struct Files {
+    pos: [u16; LEN / 2 + 1],
+    sz: [u8; LEN / 2 + 1],
+    len: usize,
+}
+
+impl Default for Files {
+    fn default() -> Self {
+        Self {
+            pos: [0; LEN / 2 + 1],
+            sz: [0; LEN / 2 + 1],
+            len: 0,
+        }
+    }
+}
+
+impl Files {
+    pub fn get(&self, index: usize) -> (u16, u8) {
+        (self.pos[index], self.sz[index])
+    }
+
+    pub fn set(&mut self, index: usize, pos: u16, sz: u8) {
+        self.pos[index] = pos;
+        self.sz[index] = sz;
+    }
+
+    pub fn push(&mut self, pos: u16, sz: u8) {
+        self.pos[self.len] = pos;
+        self.sz[self.len] = sz;
+        self.len += 1;
     }
 }
 
 fn inner_p1<const LEN: usize>(input: &[u8]) -> u64 {
+    let input = &input[..LEN];
+
     let mut disk = DiskA::default();
     for i in 0..LEN {
         if i % 2 == 0 {
@@ -114,56 +164,56 @@ fn inner_p1<const LEN: usize>(input: &[u8]) -> u64 {
 }
 
 fn inner_p2<const LEN: usize>(input: &[u8]) -> u64 {
-    let mut disk = DiskB::default();
+    let input = &input[..LEN];
+
+    let mut free = FreeBuckets::default();
+    let mut files = Files::default();
 
     let mut pos = 0;
     for i in 0..LEN {
         if i % 2 == 0 {
-            disk.push_file(pos, input[i] & 0xF, (i / 2) as u16);
-            pos += (input[i] & 0xF) as usize;
+            files.push(pos as u16, input[i] & 0xF);
         } else {
-            disk.push_free(pos, input[i] & 0xF);
-            pos += (input[i] & 0xF) as usize;
+            free.add(input[i] & 0xF, pos);
+        }
+
+        pos += (input[i] & 0xF) as usize;
+    }
+
+    for i in (0..files.len).rev() {
+        let (pos, sz) = files.get(i);
+
+        let mut min_sz = 0;
+        let mut min_pos = u16::MAX;
+        for try_sz in sz..=9 {
+            if let Some(free_pos) = free.peek(try_sz) {
+                if free_pos < min_pos {
+                    min_pos = free_pos;
+                    min_sz = try_sz;
+                }
+            }
+        }
+
+        if min_sz == 0 || min_pos > pos {
+            continue;
+        }
+
+        let free_pos = free.get(min_sz).unwrap();
+        files.set(i, free_pos, sz);
+        free.insert(sz, pos);
+        if min_sz > sz {
+            free.insert(min_sz - sz, free_pos + sz as u16);
         }
     }
 
-    for i in (0..disk.file_len).rev() {
-        let (pos, sz, id) = (disk.file_pos[i], disk.file_sz[i], disk.file_id[i]);
-
-        for j in 0..disk.free_len {
-            let (free_pos, free_sz) = (disk.free_pos[j], disk.free_sz[j]);
-            if free_pos > pos {
-                break;
-            }
-
-            if free_sz == sz {
-                disk.file_pos[i] = free_pos;
-                disk.file_sz[i] = sz;
-                disk.file_id[i] = id;
-
-                disk.free_sz[j] = 0;
-                break;
-            } else if free_sz > sz {
-                disk.file_pos[i] = free_pos;
-                disk.file_sz[i] = sz;
-                disk.file_id[i] = id;
-
-                disk.free_pos[j] = free_pos + sz as u16;
-                disk.free_sz[j] = free_sz - sz;
-                break;
-            }
+    let mut sum = 0;
+    for i in 0..files.len {
+        let (pos, sz) = files.get(i);
+        for j in pos as u64..pos as u64 + sz as u64 {
+            sum += i as u64 * j;
         }
     }
-
-    (0..disk.file_len)
-        .map(|i| {
-            let mut x = 0;
-            for j in disk.file_pos[i] as u64..disk.file_pos[i] as u64 + disk.file_sz[i] as u64 {
-                x += j * disk.file_id[i] as u64;
-            }
-            x
-        })
-        .sum()
+    sum
 }
 
 pub fn part1(input: &str) -> u64 {
