4307edef51c2d1863bf932ed5e14823745a581b7
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-12-11 20:28:03 -0500

    fallback if >8 digit
    surely they wont modify the inputs

--- src/day11.rs
+++ src/day11.rs
@@ -1,5 +1,78 @@
+use std::collections::HashMap;
+
+use rustc_hash::FxBuildHasher;
+
 const LUT: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/day11_lut.bin"));
 const LUT_STONE_COUNT: u64 = 10000000;
+
+struct Memo {
+    data: HashMap<(u64, usize), u64, FxBuildHasher>,
+}
+
+impl Memo {
+    pub fn new(cap: usize) -> Self {
+        Self {
+            data: HashMap::with_capacity_and_hasher(cap, FxBuildHasher::default()),
+        }
+    }
+
+    #[inline]
+    pub fn get(&mut self, stone: u64, steps: usize) -> Option<&u64> {
+        self.data.get(&(stone, steps))
+    }
+
+    #[inline]
+    pub fn set(&mut self, stone: u64, steps: usize, substones: u64) {
+        self.data.insert((stone, steps), substones);
+    }
+}
+
+// https://da-data.blogspot.com/2023/02/integer-log10-in-rust-and-c.html
+// adapted for u64
+#[inline]
+fn fast_ilog10(x: u64) -> u32 {
+    #[rustfmt::skip]
+    const LUT: [u64; 15] = [
+        9,                  99,                 999,
+        9_999,              99_999,             999_999,
+        9_999_999,          99_999_999,         999_999_999,
+        9_999_999_999,      99_999_999_999,     999_999_999_999,
+        9_999_999_999_999,  99_999_999_999_999, 999_999_999_999_999,
+    ];
+
+    const MASK: u64 = 0b0001001001000100100100010010010001001001000100100100010010010000;
+
+    let guess = (MASK << x.leading_zeros()).count_ones() as usize;
+    guess as u32 + (x > LUT[guess]) as u32
+}
+
+fn calculate_inner(memo: &mut Memo, stone: u64, steps: usize) -> u64 {
+    if let Some(result) = memo.get(stone, steps) {
+        *result
+    } else if steps == 0 {
+        1
+    } else {
+        let result = if stone == 0 {
+            calculate_inner(memo, 1, steps - 1)
+        } else {
+            let log10 = fast_ilog10(stone);
+
+            if log10 % 2 == 1 {
+                let pow = 10u64.pow(log10 / 2 + 1);
+
+                let lhs = calculate_inner(memo, stone / pow, steps - 1);
+                let rhs = calculate_inner(memo, stone % pow, steps - 1);
+
+                lhs + rhs
+            } else {
+                calculate_inner(memo, stone * 2024, steps - 1)
+            }
+        };
+
+        memo.set(stone, steps, result);
+        result
+    }
+}
 
 fn lut_lookup<const P1: bool>(stone: u64) -> Option<u64> {
     if stone >= LUT_STONE_COUNT {
@@ -18,10 +91,15 @@ fn calculate<const P1: bool>(input: &str) -> u64 {
     let input = input.as_bytes();
     let end = memchr::memchr(b'\n', input).unwrap_or(input.len());
 
+    let mut memo = Memo::new(if P1 { 6400 } else { 160000 });
+
     let mut pos = 0;
     let mut sum = 0;
     for delim in memchr::memchr_iter(b' ', input) {
-        sum += lut_lookup::<P1>(parse_num(&input[pos..delim])).unwrap();
+        let stone = parse_num(&input[pos..delim]);
+
+        sum += lut_lookup::<P1>(stone)
+            .unwrap_or_else(|| calculate_inner(&mut memo, stone, if P1 { 25 } else { 75 }));
         pos = delim + 1;
     }
 
