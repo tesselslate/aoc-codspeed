9f147b45ce8926b853fe4eb59120c6e40b875d23
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-12-11 19:47:06 -0500

    75 functions
    its not faster today unfortunately

--- src/day11.rs
+++ src/day11.rs
@@ -4,32 +4,17 @@ use rustc_hash::FxBuildHasher;
 
 struct Memo {
     data: HashMap<(u64, usize), u64, FxBuildHasher>,
-    misses: usize,
-    hits: usize,
 }
 
 impl Memo {
     pub fn new(cap: usize) -> Self {
         Self {
             data: HashMap::with_capacity_and_hasher(cap, FxBuildHasher::default()),
-            misses: 0,
-            hits: 0,
         }
     }
 
     #[inline]
     pub fn get(&mut self, stone: u64, steps: usize) -> Option<&u64> {
-        #[cfg(debug_assertions)]
-        {
-            if let Some(x) = self.data.get(&(stone, steps)) {
-                self.hits += 1;
-                return Some(x);
-            } else {
-                self.misses += 1;
-                return None;
-            }
-        }
-
         self.data.get(&(stone, steps))
     }
 
@@ -58,33 +43,116 @@ fn fast_ilog10(x: u64) -> u32 {
     guess as u32 + (x > LUT[guess]) as u32
 }
 
-fn calculate(memo: &mut Memo, stone: u64, steps: usize) -> u64 {
-    if let Some(result) = memo.get(stone, steps) {
-        *result
-    } else if steps == 0 {
-        1
-    } else {
-        let result = if stone == 0 {
-            calculate(memo, 1, steps - 1)
-        } else {
-            let log10 = fast_ilog10(stone);
-
-            if log10 % 2 == 1 {
-                let pow = 10u64.pow(log10 / 2 + 1);
-
-                let lhs = calculate(memo, stone / pow, steps - 1);
-                let rhs = calculate(memo, stone % pow, steps - 1);
-
-                lhs + rhs
+macro_rules! recurse_impl {
+    ($func_name: ident, $func_next: ident, $steps: literal) => {
+        fn $func_name(memo: &mut Memo, stone: u64) -> u64 {
+            if let Some(result) = memo.get(stone, $steps) {
+                *result
             } else {
-                calculate(memo, stone * 2024, steps - 1)
-            }
-        };
+                let result = if stone == 0 {
+                    $func_next(memo, 1)
+                } else {
+                    let log10 = fast_ilog10(stone);
 
-        memo.set(stone, steps, result);
-        result
-    }
+                    if log10 % 2 == 1 {
+                        let pow = 10u64.pow(log10 / 2 + 1);
+
+                        let lhs = $func_next(memo, stone / pow);
+                        let rhs = $func_next(memo, stone % pow);
+
+                        lhs + rhs
+                    } else {
+                        $func_next(memo, stone * 2024)
+                    }
+                };
+
+                memo.set(stone, $steps, result);
+                result
+            }
+        }
+    };
 }
+
+#[inline(always)]
+fn stone_0(_: &mut Memo, _: u64) -> u64 {
+    1
+}
+
+recurse_impl!(stone_1, stone_0, 1);
+recurse_impl!(stone_2, stone_1, 2);
+recurse_impl!(stone_3, stone_2, 3);
+recurse_impl!(stone_4, stone_3, 4);
+recurse_impl!(stone_5, stone_4, 5);
+recurse_impl!(stone_6, stone_5, 6);
+recurse_impl!(stone_7, stone_6, 7);
+recurse_impl!(stone_8, stone_7, 8);
+recurse_impl!(stone_9, stone_8, 9);
+recurse_impl!(stone_10, stone_9, 10);
+recurse_impl!(stone_11, stone_10, 11);
+recurse_impl!(stone_12, stone_11, 12);
+recurse_impl!(stone_13, stone_12, 13);
+recurse_impl!(stone_14, stone_13, 14);
+recurse_impl!(stone_15, stone_14, 15);
+recurse_impl!(stone_16, stone_15, 16);
+recurse_impl!(stone_17, stone_16, 17);
+recurse_impl!(stone_18, stone_17, 18);
+recurse_impl!(stone_19, stone_18, 19);
+recurse_impl!(stone_20, stone_19, 20);
+recurse_impl!(stone_21, stone_20, 21);
+recurse_impl!(stone_22, stone_21, 22);
+recurse_impl!(stone_23, stone_22, 23);
+recurse_impl!(stone_24, stone_23, 24);
+recurse_impl!(stone_25, stone_24, 25);
+recurse_impl!(stone_26, stone_25, 26);
+recurse_impl!(stone_27, stone_26, 27);
+recurse_impl!(stone_28, stone_27, 28);
+recurse_impl!(stone_29, stone_28, 29);
+recurse_impl!(stone_30, stone_29, 30);
+recurse_impl!(stone_31, stone_30, 31);
+recurse_impl!(stone_32, stone_31, 32);
+recurse_impl!(stone_33, stone_32, 33);
+recurse_impl!(stone_34, stone_33, 34);
+recurse_impl!(stone_35, stone_34, 35);
+recurse_impl!(stone_36, stone_35, 36);
+recurse_impl!(stone_37, stone_36, 37);
+recurse_impl!(stone_38, stone_37, 38);
+recurse_impl!(stone_39, stone_38, 39);
+recurse_impl!(stone_40, stone_39, 40);
+recurse_impl!(stone_41, stone_40, 41);
+recurse_impl!(stone_42, stone_41, 42);
+recurse_impl!(stone_43, stone_42, 43);
+recurse_impl!(stone_44, stone_43, 44);
+recurse_impl!(stone_45, stone_44, 45);
+recurse_impl!(stone_46, stone_45, 46);
+recurse_impl!(stone_47, stone_46, 47);
+recurse_impl!(stone_48, stone_47, 48);
+recurse_impl!(stone_49, stone_48, 49);
+recurse_impl!(stone_50, stone_49, 50);
+recurse_impl!(stone_51, stone_50, 51);
+recurse_impl!(stone_52, stone_51, 52);
+recurse_impl!(stone_53, stone_52, 53);
+recurse_impl!(stone_54, stone_53, 54);
+recurse_impl!(stone_55, stone_54, 55);
+recurse_impl!(stone_56, stone_55, 56);
+recurse_impl!(stone_57, stone_56, 57);
+recurse_impl!(stone_58, stone_57, 58);
+recurse_impl!(stone_59, stone_58, 59);
+recurse_impl!(stone_60, stone_59, 60);
+recurse_impl!(stone_61, stone_60, 61);
+recurse_impl!(stone_62, stone_61, 62);
+recurse_impl!(stone_63, stone_62, 63);
+recurse_impl!(stone_64, stone_63, 64);
+recurse_impl!(stone_65, stone_64, 65);
+recurse_impl!(stone_66, stone_65, 66);
+recurse_impl!(stone_67, stone_66, 67);
+recurse_impl!(stone_68, stone_67, 68);
+recurse_impl!(stone_69, stone_68, 69);
+recurse_impl!(stone_70, stone_69, 70);
+recurse_impl!(stone_71, stone_70, 71);
+recurse_impl!(stone_72, stone_71, 72);
+recurse_impl!(stone_73, stone_72, 73);
+recurse_impl!(stone_74, stone_73, 74);
+recurse_impl!(stone_75, stone_74, 75);
 
 fn calculate_outer<const STEPS: usize>(input: &str) -> u64 {
     let input = input.as_bytes();
@@ -111,12 +179,11 @@ fn calculate_outer<const STEPS: usize>(input: &str) -> u64 {
     let mut memo = Memo::new(expect_cap);
     let mut sum = 0;
     for &stone in &stones[..num_stones] {
-        sum += calculate(&mut memo, stone, STEPS);
-    }
-
-    #[cfg(debug_assertions)]
-    {
-        println!("{}/{} cache accesses", memo.hits, memo.hits + memo.misses);
+        if STEPS == 25 {
+            sum += stone_25(&mut memo, stone);
+        } else if STEPS == 75 {
+            sum += stone_75(&mut memo, stone);
+        }
     }
 
     sum
