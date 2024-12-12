67b73b58b2e00aef708f7a051d69803528d00b44
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-12-11 22:19:50 -0500

    use alternative approach for part 2
    regresses part 1 by ~5us because Rust Compiler but improves p2 by like
    50 so its still a small improvement

--- src/day11.rs
+++ src/day11.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{hash_map, HashMap};
 
 use rustc_hash::FxBuildHasher;
 
@@ -7,9 +7,9 @@ struct Memo {
 }
 
 impl Memo {
-    pub fn new(cap: usize) -> Self {
+    pub fn new() -> Self {
         Self {
-            data: HashMap::with_capacity_and_hasher(cap, FxBuildHasher::default()),
+            data: HashMap::with_capacity_and_hasher(160000, FxBuildHasher::default()),
         }
     }
 
@@ -21,6 +21,38 @@ impl Memo {
     #[inline]
     pub fn set(&mut self, stone: u64, steps: usize, substones: u64) {
         self.data.insert((stone, steps), substones);
+    }
+}
+
+struct Stones {
+    data: HashMap<u64, u64, FxBuildHasher>,
+}
+
+impl Stones {
+    pub fn new() -> Self {
+        Self {
+            data: HashMap::with_capacity_and_hasher(4096, FxBuildHasher::default()),
+        }
+    }
+
+    #[inline]
+    pub fn clear(&mut self) {
+        self.data.clear();
+    }
+
+    #[inline]
+    pub fn inc(&mut self, stone: u64, count: u64) {
+        *self.data.entry(stone).or_insert(0) += count;
+    }
+
+    #[inline]
+    pub fn iter(&self) -> hash_map::Iter<u64, u64> {
+        self.data.iter()
+    }
+
+    #[inline]
+    pub fn sum(&self) -> u64 {
+        self.data.values().sum()
     }
 }
 
@@ -43,26 +75,26 @@ fn fast_ilog10(x: u64) -> u32 {
     guess as u32 + (x > LUT[guess]) as u32
 }
 
-fn calculate(memo: &mut Memo, stone: u64, steps: usize) -> u64 {
+fn inner_p1(memo: &mut Memo, stone: u64, steps: usize) -> u64 {
     if let Some(result) = memo.get(stone, steps) {
         *result
     } else if steps == 0 {
         1
     } else {
         let result = if stone == 0 {
-            calculate(memo, 1, steps - 1)
+            inner_p1(memo, 1, steps - 1)
         } else {
             let log10 = fast_ilog10(stone);
 
             if log10 % 2 == 1 {
                 let pow = 10u64.pow(log10 / 2 + 1);
 
-                let lhs = calculate(memo, stone / pow, steps - 1);
-                let rhs = calculate(memo, stone % pow, steps - 1);
+                let lhs = inner_p1(memo, stone / pow, steps - 1);
+                let rhs = inner_p1(memo, stone % pow, steps - 1);
 
                 lhs + rhs
             } else {
-                calculate(memo, stone * 2024, steps - 1)
+                inner_p1(memo, stone * 2024, steps - 1)
             }
         };
 
@@ -71,35 +103,61 @@ fn calculate(memo: &mut Memo, stone: u64, steps: usize) -> u64 {
     }
 }
 
-fn calculate_outer<const STEPS: usize>(input: &str) -> u64 {
+fn calculate_p1(input: &str) -> u64 {
     let input = input.as_bytes();
     let end = memchr::memchr(b'\n', input).unwrap_or(input.len());
 
-    let mut stones = [0u64; 16];
-    let mut num_stones = 0;
+    let mut memo = Memo::new();
+    let mut sum = 0;
+
     let mut pos = 0;
-
     for delim in memchr::memchr_iter(b' ', input) {
-        let bytes = &input[pos..delim];
-
-        stones[num_stones] = parse_num(bytes);
-        num_stones += 1;
-
+        sum += inner_p1(&mut memo, parse_num(&input[pos..delim]), 25);
         pos = delim + 1;
     }
-
-    stones[num_stones] = parse_num(&input[pos..end]);
-    num_stones += 1;
-
-    let expect_cap = if STEPS == 25 { 6400 } else { 160000 };
-
-    let mut memo = Memo::new(expect_cap);
-    let mut sum = 0;
-    for &stone in &stones[..num_stones] {
-        sum += calculate(&mut memo, stone, STEPS);
-    }
+    sum += inner_p1(&mut memo, parse_num(&input[pos..end]), 25);
 
     sum
+}
+
+fn calculate_p2(input: &str) -> u64 {
+    let input = input.as_bytes();
+    let end = memchr::memchr(b'\n', input).unwrap_or(input.len());
+
+    let mut a = Stones::new();
+    let mut b = Stones::new();
+
+    let mut pos = 0;
+    for delim in memchr::memchr_iter(b' ', input) {
+        a.inc(parse_num(&input[pos..delim]), 1);
+        pos = delim + 1;
+    }
+    a.inc(parse_num(&input[pos..end]), 1);
+
+    for _ in 0..75 {
+        b.clear();
+
+        for (&stone, &count) in a.iter() {
+            if stone == 0 {
+                b.inc(1, count);
+            } else {
+                let log = fast_ilog10(stone);
+
+                if log % 2 == 1 {
+                    let pow = 10u64.pow(log / 2 + 1);
+
+                    b.inc(stone / pow, count);
+                    b.inc(stone % pow, count);
+                } else {
+                    b.inc(stone * 2024, count);
+                }
+            }
+        }
+
+        std::mem::swap(&mut a, &mut b);
+    }
+
+    a.sum()
 }
 
 fn parse_num(b: &[u8]) -> u64 {
@@ -107,11 +165,11 @@ fn parse_num(b: &[u8]) -> u64 {
 }
 
 pub fn part1(input: &str) -> u64 {
-    calculate_outer::<25>(input)
+    calculate_p1(input)
 }
 
 pub fn part2(input: &str) -> u64 {
-    calculate_outer::<75>(input)
+    calculate_p2(input)
 }
 
 #[cfg(test)]
