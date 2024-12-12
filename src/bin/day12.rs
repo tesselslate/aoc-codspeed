9292b3ff36a825c12cda85029dcd9073163b35d5
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-12-12 18:30:19 -0500

    day 12 bin

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -1,13 +1,13 @@
 use std::time::Instant;
 
-use aoc_codspeed::day11;
+use aoc_codspeed::day12;
 
-const INPUT: &str = include_str!("../../inputs/input11.txt");
+const INPUT: &str = include_str!("../../inputs/input12.txt");
 
 fn main() {
     let start = Instant::now();
     for _ in 0..1000 {
-        std::hint::black_box(day11::part1(INPUT));
+        std::hint::black_box(day12::part1(INPUT));
     }
     println!(
         "p1: {:.3} usec",
@@ -16,7 +16,7 @@ fn main() {
 
     let start = Instant::now();
     for _ in 0..1000 {
-        std::hint::black_box(day11::part2(INPUT));
+        std::hint::black_box(day12::part2(INPUT));
     }
     println!(
         "p2: {:.3} usec",
