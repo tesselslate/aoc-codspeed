6484638f68c86e393914de6d74d9a7989373faef
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-12-14 04:03:00 -0500

    raise iteration limit on day 13 bin

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -5,21 +5,22 @@ use aoc_codspeed::day13;
 const INPUT: &str = include_str!("../../inputs/input13.txt");
 
 fn main() {
+    const N: usize = 10000000;
     let start = Instant::now();
-    for _ in 0..1000 {
+    for _ in 0..N {
         std::hint::black_box(day13::part1(INPUT));
     }
     println!(
         "p1: {:.3} usec",
-        Instant::now().duration_since(start).as_nanos() as f64 / 1000.0 / 1000.0
+        Instant::now().duration_since(start).as_nanos() as f64 / 1000.0 / N as f64
     );
 
     let start = Instant::now();
-    for _ in 0..1000 {
+    for _ in 0..N {
         std::hint::black_box(day13::part2(INPUT));
     }
     println!(
         "p2: {:.3} usec",
-        Instant::now().duration_since(start).as_nanos() as f64 / 1000.0 / 1000.0
+        Instant::now().duration_since(start).as_nanos() as f64 / 1000.0 / N as f64
     );
 }
