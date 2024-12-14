5ae7d84391308afacc4d59f446293e7ed26a6293
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-12-14 01:53:25 -0500

    use inline asm for day 13
    this problem seemed viable enough to do with inline assembly and it is!
    this is my first time writing x86 asm that's not targetting
    real/protected mode, so that's cool

--- src/day13.rs
+++ src/day13.rs
@@ -1,93 +1,423 @@
-struct Scenario([i64; 6]);
-
-impl Default for Scenario {
-    fn default() -> Self {
-        Self([0; 6])
-    }
-}
-
-#[inline]
-fn solve(scenario: &Scenario) -> Option<i64> {
-    let [ax, ay, bx, by, px, py] = scenario.0;
-
-    let div = ax * by - ay * bx;
-    unsafe { std::hint::assert_unchecked(div != 0) };
-
-    let a = (px * by - py * bx) / div;
-    let b = (py * ax - px * ay) / div;
-
-    if a * ax + b * bx == px && a * ay + b * by == py {
-        Some(3 * a + b)
-    } else {
-        None
-    }
-}
-
-#[inline(always)]
-unsafe fn read_i64_2(ptr: *const u8, acc: &mut i64) {
-    let a = (*ptr - b'0') as i64;
-    let b = (*ptr.add(1) - b'0') as i64;
-    *acc = a * 10 + b;
-}
-
-#[inline]
-unsafe fn read_i64_3p<const DELIM: u8>(ptr: *const u8, acc: &mut i64) -> *const u8 {
-    let a = (*ptr - b'0') as i64;
-    let b = (*ptr.add(1) - b'0') as i64;
-    let c = (*ptr.add(2) - b'0') as i64;
-
-    if *ptr.add(3) == DELIM {
-        *acc = a * 100 + b * 10 + c;
-        ptr.add(3)
-    } else if *ptr.add(4) == DELIM {
-        let d = (*ptr.add(3) - b'0') as i64;
-
-        *acc = a * 1000 + b * 100 + c * 10 + d;
-        ptr.add(4)
-    } else {
-        let d = (*ptr.add(3) - b'0') as i64;
-        let e = (*ptr.add(4) - b'0') as i64;
-
-        *acc = a * 10000 + b * 1000 + c * 100 + d * 10 + e;
-        ptr.add(5)
-    }
-}
-
-unsafe fn inner<const N: usize, const D: i64>(input: &[u8]) -> i64 {
-    const SKIP_BUTTON_AX: usize = "Button A: X+".len();
-    const SKIP_BUTTON_AY: usize = "Button A: X+XX, Y+".len();
-    const SKIP_BUTTON_BX: usize = "Button A: X+XX, Y+YY\nButton B: X+".len();
-    const SKIP_BUTTON_BY: usize = "Button A: X+XX, Y+YY\nButton B: X+XX, Y+".len();
-    const SKIP_PRIZE_X: usize = "Button A: X+XX, Y+YY\nButton B: X+XX, Y+YY\nPrize: X=".len();
-
+unsafe fn p1_inner(input: *const u8) -> u64 {
     let mut sum = 0;
-    let mut scenario = Scenario::default();
 
-    let mut ptr = input.as_ptr();
-    for _ in 0..N {
-        read_i64_2(ptr.add(SKIP_BUTTON_AX), &mut scenario.0[0]);
-        read_i64_2(ptr.add(SKIP_BUTTON_AY), &mut scenario.0[1]);
+    core::arch::asm!(
+    // set loop counter
+        "push 320",
+    "20:", // load ax
+        "movzx {b1}, byte ptr [{inp}+12]",
+        "movzx {b2}, byte ptr [{inp}+13]",
+        "and {b1}, 0xF",
+        "and {b2}, 0xF",
+        "add {b1}, {b1}",
+        "lea {b1}, [{b1} + 4*{b1}]",
+        "add {b1}, {b2}",
+        "mov {ax}, {b1}",
+    // load ay
+        "movzx {b1}, byte ptr [{inp}+18]",
+        "movzx {b2}, byte ptr [{inp}+19]",
+        "and {b1}, 0xF",
+        "and {b2}, 0xF",
+        "add {b1}, {b1}",
+        "lea {b1}, [{b1} + 4*{b1}]",
+        "add {b1}, {b2}",
+        "mov {ay}, {b1}",
+    // load bx
+        "movzx {b1}, byte ptr [{inp}+33]",
+        "movzx {b2}, byte ptr [{inp}+34]",
+        "and {b1}, 0xF",
+        "and {b2}, 0xF",
+        "add {b1}, {b1}",
+        "lea {b1}, [{b1} + 4*{b1}]",
+        "add {b1}, {b2}", "mov {bx}, {b1}",
+    // load by
+        "movzx {b1}, byte ptr [{inp}+39]",
+        "movzx {b2}, byte ptr [{inp}+40]",
+        "and {b1}, 0xF",
+        "and {b2}, 0xF",
+        "add {b1}, {b1}",
+        "lea {b1}, [{b1} + 4*{b1}]",
+        "add {b1}, {b2}",
+        "mov {by}, {b1}",
+    // load tx
+        "xor {tx}, {tx}",
+        "add {inp}, 51",
+        "movzx {b1}, byte ptr [{inp}]",
+        "movzx {b2}, byte ptr [{inp}+1]",
+        "movzx {b3}, byte ptr [{inp}+2]",
+        "movzx {b4}, byte ptr [{inp}+3]",
+        "cmp {b4}, 0x2C",
+        "je 23f",
+        "movzx {b5}, byte ptr [{inp}+4]",
+        "cmp {b5}, 0x2C",
+        "je 24f",
+    // load tx (5 digits)
+        "and {b1}, 0xF",
+        "and {b2}, 0xF",
+        "and {b3}, 0xF",
+        "and {b4}, 0xF",
+        "and {b5}, 0xF",
+        "imul {b1}, 10000",
+        "imul {b2}, 1000",
+        "imul {b3}, 100",
+        "imul {b4}, 10",
+        "add {tx}, {b1}",
+        "add {tx}, {b2}",
+        "add {tx}, {b3}",
+        "add {tx}, {b4}",
+        "add {tx}, {b5}",
+        "add {inp}, 9",
+        "jmp 30f",
+    "23:", // load tx (3 digits)
+        "and {b1}, 0xF",
+        "and {b2}, 0xF",
+        "and {b3}, 0xF",
+        "imul {b1}, 100",
+        "imul {b2}, 10",
+        "add {tx}, {b1}",
+        "add {tx}, {b2}",
+        "add {tx}, {b3}",
+        "add {inp}, 7",
+        "jmp 30f",
+    "24:", // load tx (4 digits)
+        "and {b1}, 0xF",
+        "and {b2}, 0xF",
+        "and {b3}, 0xF",
+        "and {b4}, 0xF",
+        "imul {b1}, 1000",
+        "imul {b2}, 100",
+        "imul {b3}, 10",
+        "add {tx}, {b1}",
+        "add {tx}, {b2}",
+        "add {tx}, {b3}",
+        "add {tx}, {b4}",
+        "add {inp}, 8",
+    "30:", // load ty
+        "xor {ty}, {ty}",
+        "movzx {b1}, byte ptr [{inp}]",
+        "movzx {b2}, byte ptr [{inp}+1]",
+        "movzx {b3}, byte ptr [{inp}+2]",
+        "movzx {b4}, byte ptr [{inp}+3]",
+        "cmp {b4}, 0x0A",
+        "je 33f",
+        "movzx {b5}, byte ptr [{inp}+4]",
+        "cmp {b5}, 0x0A",
+        "je 34f",
+    // load ty (5 digits)
+        "and {b1}, 0xF",
+        "and {b2}, 0xF",
+        "and {b3}, 0xF",
+        "and {b4}, 0xF",
+        "and {b5}, 0xF",
+        "imul {b1}, 10000",
+        "imul {b2}, 1000",
+        "imul {b3}, 100",
+        "imul {b4}, 10",
+        "add {ty}, {b1}",
+        "add {ty}, {b2}",
+        "add {ty}, {b3}",
+        "add {ty}, {b4}",
+        "add {ty}, {b5}",
+        "add {inp}, 7",
+        "jmp 40f",
+    "33:", // load ty (3 digits)
+        "and {b1}, 0xF",
+        "and {b2}, 0xF",
+        "and {b3}, 0xF",
+        "imul {b1}, 100",
+        "imul {b2}, 10",
+        "add {ty}, {b1}",
+        "add {ty}, {b2}",
+        "add {ty}, {b3}",
+        "add {inp}, 5",
+        "jmp 40f",
+    "34:", // load ty (4 digits)
+        "and {b1}, 0xF",
+        "and {b2}, 0xF",
+        "and {b3}, 0xF",
+        "and {b4}, 0xF",
+        "imul {b1}, 1000",
+        "imul {b2}, 100",
+        "imul {b3}, 10",
+        "add {ty}, {b1}",
+        "add {ty}, {b2}",
+        "add {ty}, {b3}",
+        "add {ty}, {b4}",
+        "add {inp}, 6",
+    "40:", // calculate score and add to sum
+        "mov {b1}, {ax}",
+        "imul {b1}, {by}",
+        "mov {b4}, {ay}",
+        "imul {b4}, {bx}",
+        "sub {b1}, {b4}",
 
-        read_i64_2(ptr.add(SKIP_BUTTON_BX), &mut scenario.0[2]);
-        read_i64_2(ptr.add(SKIP_BUTTON_BY), &mut scenario.0[3]);
+        "xor rax, rax",
+        "mov rax, {tx}",
+        "imul rax, {by}",
+        "mov rdx, {ty}",
+        "imul rdx, {bx}",
+        "sub rax, rdx",
+        "cqo",
+        "idiv {b1}",
+        "test rdx, rdx",
+        "jnz 50f",
+        "mov {b2}, rax",
 
-        ptr = read_i64_3p::<b','>(ptr.add(SKIP_PRIZE_X), &mut scenario.0[4]);
-        ptr = read_i64_3p::<b'\n'>(ptr.add(4), &mut scenario.0[5]).add(2);
+        "mov rax, {ty}",
+        "imul rax, {ax}",
+        "mov rdx, {tx}",
+        "imul rdx, {ay}",
+        "sub rax, rdx",
+        "cqo",
+        "idiv {b1}",
+        "test rdx, rdx",
+        "jnz 50f",
 
-        scenario.0[4] += D;
-        scenario.0[5] += D;
-        sum += solve(&scenario).unwrap_or(0);
-    }
+        "add {sum}, rax",
+        "imul {b2}, 3",
+        "add {sum}, {b2}",
+    "50:", // loop
+        "pop rax",
+        "sub rax, 1",
+        "push rax",
+        "jnz 20b",
+        "pop rax",
+
+        sum = inout(reg) sum,
+        inp = inout(reg) input => _,
+        b1 = out(reg) _, // divisor
+        b2 = out(reg) _, // a
+        b3 = out(reg) _,
+        b4 = out(reg) _,
+        b5 = out(reg) _,
+        ax = out(reg) _,
+        ay = out(reg) _,
+        bx = out(reg) _,
+        by = out(reg) _,
+        tx = out(reg) _,
+        ty = out(reg) _,
+        out("rax") _,
+        out("rdx") _,
+    );
 
     sum
 }
 
-pub fn part1(input: &str) -> i64 {
-    unsafe { inner::<320, 0>(input.as_bytes()) }
+unsafe fn p2_inner(input: *const u8) -> u64 {
+    let mut sum = 0;
+
+    core::arch::asm!(
+    // set loop counter
+        "push 320",
+    "20:", // load ax
+        "movzx {b1}, byte ptr [{inp}+12]",
+        "movzx {b2}, byte ptr [{inp}+13]",
+        "and {b1}, 0xF",
+        "and {b2}, 0xF",
+        "add {b1}, {b1}",
+        "lea {b1}, [{b1} + 4*{b1}]",
+        "add {b1}, {b2}",
+        "mov {ax}, {b1}",
+    // load ay
+        "movzx {b1}, byte ptr [{inp}+18]",
+        "movzx {b2}, byte ptr [{inp}+19]",
+        "and {b1}, 0xF",
+        "and {b2}, 0xF",
+        "add {b1}, {b1}",
+        "lea {b1}, [{b1} + 4*{b1}]",
+        "add {b1}, {b2}",
+        "mov {ay}, {b1}",
+    // load bx
+        "movzx {b1}, byte ptr [{inp}+33]",
+        "movzx {b2}, byte ptr [{inp}+34]",
+        "and {b1}, 0xF",
+        "and {b2}, 0xF",
+        "add {b1}, {b1}",
+        "lea {b1}, [{b1} + 4*{b1}]",
+        "add {b1}, {b2}", "mov {bx}, {b1}",
+    // load by
+        "movzx {b1}, byte ptr [{inp}+39]",
+        "movzx {b2}, byte ptr [{inp}+40]",
+        "and {b1}, 0xF",
+        "and {b2}, 0xF",
+        "add {b1}, {b1}",
+        "lea {b1}, [{b1} + 4*{b1}]",
+        "add {b1}, {b2}",
+        "mov {by}, {b1}",
+    // load tx
+        "xor {tx}, {tx}",
+        "add {inp}, 51",
+        "movzx {b1}, byte ptr [{inp}]",
+        "movzx {b2}, byte ptr [{inp}+1]",
+        "movzx {b3}, byte ptr [{inp}+2]",
+        "movzx {b4}, byte ptr [{inp}+3]",
+        "cmp {b4}, 0x2C",
+        "je 23f",
+        "movzx {b5}, byte ptr [{inp}+4]",
+        "cmp {b5}, 0x2C",
+        "je 24f",
+    // load tx (5 digits)
+        "and {b1}, 0xF",
+        "and {b2}, 0xF",
+        "and {b3}, 0xF",
+        "and {b4}, 0xF",
+        "and {b5}, 0xF",
+        "imul {b1}, 10000",
+        "imul {b2}, 1000",
+        "imul {b3}, 100",
+        "imul {b4}, 10",
+        "add {tx}, {b1}",
+        "add {tx}, {b2}",
+        "add {tx}, {b3}",
+        "add {tx}, {b4}",
+        "add {tx}, {b5}",
+        "add {inp}, 9",
+        "jmp 30f",
+    "23:", // load tx (3 digits)
+        "and {b1}, 0xF",
+        "and {b2}, 0xF",
+        "and {b3}, 0xF",
+        "imul {b1}, 100",
+        "imul {b2}, 10",
+        "add {tx}, {b1}",
+        "add {tx}, {b2}",
+        "add {tx}, {b3}",
+        "add {inp}, 7",
+        "jmp 30f",
+    "24:", // load tx (4 digits)
+        "and {b1}, 0xF",
+        "and {b2}, 0xF",
+        "and {b3}, 0xF",
+        "and {b4}, 0xF",
+        "imul {b1}, 1000",
+        "imul {b2}, 100",
+        "imul {b3}, 10",
+        "add {tx}, {b1}",
+        "add {tx}, {b2}",
+        "add {tx}, {b3}",
+        "add {tx}, {b4}",
+        "add {inp}, 8",
+    "30:", // load ty
+        "mov rax, 10000000000000",
+        "add {tx}, rax",
+        "xor {ty}, {ty}",
+        "movzx {b1}, byte ptr [{inp}]",
+        "movzx {b2}, byte ptr [{inp}+1]",
+        "movzx {b3}, byte ptr [{inp}+2]",
+        "movzx {b4}, byte ptr [{inp}+3]",
+        "cmp {b4}, 0x0A",
+        "je 33f",
+        "movzx {b5}, byte ptr [{inp}+4]",
+        "cmp {b5}, 0x0A",
+        "je 34f",
+    // load ty (5 digits)
+        "and {b1}, 0xF",
+        "and {b2}, 0xF",
+        "and {b3}, 0xF",
+        "and {b4}, 0xF",
+        "and {b5}, 0xF",
+        "imul {b1}, 10000",
+        "imul {b2}, 1000",
+        "imul {b3}, 100",
+        "imul {b4}, 10",
+        "add {ty}, {b1}",
+        "add {ty}, {b2}",
+        "add {ty}, {b3}",
+        "add {ty}, {b4}",
+        "add {ty}, {b5}",
+        "add {inp}, 7",
+        "jmp 40f",
+    "33:", // load ty (3 digits)
+        "and {b1}, 0xF",
+        "and {b2}, 0xF",
+        "and {b3}, 0xF",
+        "imul {b1}, 100",
+        "imul {b2}, 10",
+        "add {ty}, {b1}",
+        "add {ty}, {b2}",
+        "add {ty}, {b3}",
+        "add {inp}, 5",
+        "jmp 40f",
+    "34:", // load ty (4 digits)
+        "and {b1}, 0xF",
+        "and {b2}, 0xF",
+        "and {b3}, 0xF",
+        "and {b4}, 0xF",
+        "imul {b1}, 1000",
+        "imul {b2}, 100",
+        "imul {b3}, 10",
+        "add {ty}, {b1}",
+        "add {ty}, {b2}",
+        "add {ty}, {b3}",
+        "add {ty}, {b4}",
+        "add {inp}, 6",
+    "40:", // calculate score and add to sum
+        "mov rax, 10000000000000",
+        "add {ty}, rax",
+        "mov {b1}, {ax}",
+        "imul {b1}, {by}",
+        "mov {b4}, {ay}",
+        "imul {b4}, {bx}",
+        "sub {b1}, {b4}",
+
+        "xor rax, rax",
+        "mov rax, {tx}",
+        "imul rax, {by}",
+        "mov rdx, {ty}",
+        "imul rdx, {bx}",
+        "sub rax, rdx",
+        "cqo",
+        "idiv {b1}",
+        "test rdx, rdx",
+        "jnz 50f",
+        "mov {b2}, rax",
+
+        "mov rax, {ty}",
+        "imul rax, {ax}",
+        "mov rdx, {tx}",
+        "imul rdx, {ay}",
+        "sub rax, rdx",
+        "cqo",
+        "idiv {b1}",
+        "test rdx, rdx",
+        "jnz 50f",
+
+        "add {sum}, rax",
+        "imul {b2}, 3",
+        "add {sum}, {b2}",
+    "50:", // loop
+        "pop rax",
+        "sub rax, 1",
+        "push rax",
+        "jnz 20b",
+        "pop rax",
+
+        sum = inout(reg) sum,
+        inp = inout(reg) input => _,
+        b1 = out(reg) _, // divisor
+        b2 = out(reg) _, // a
+        b3 = out(reg) _,
+        b4 = out(reg) _,
+        b5 = out(reg) _,
+        ax = out(reg) _,
+        ay = out(reg) _,
+        bx = out(reg) _,
+        by = out(reg) _,
+        tx = out(reg) _,
+        ty = out(reg) _,
+        out("rax") _,
+        out("rdx") _,
+    );
+
+    sum
 }
 
-pub fn part2(input: &str) -> i64 {
-    unsafe { inner::<320, 10000000000000>(input.as_bytes()) }
+pub fn part1(input: &str) -> u64 {
+    unsafe { p1_inner(input.as_ptr()) }
+}
+
+pub fn part2(input: &str) -> u64 {
+    unsafe { p2_inner(input.as_ptr()) }
 }
 
 #[cfg(test)]
@@ -95,7 +425,6 @@ mod tests {
     use super::*;
 
     const INPUT: &str = include_str!("../inputs/input13.txt");
-    const TEST: &str = include_str!("../testdata/input13.txt");
 
     #[test]
     fn a() {
@@ -105,18 +434,5 @@ mod tests {
     #[test]
     fn b() {
         assert_eq!(part2(INPUT), 104140871044942);
-    }
-
-    #[test]
-    fn test_a() {
-        assert_eq!(unsafe { inner::<4, 0>(TEST.as_bytes()) }, 480);
-    }
-
-    #[test]
-    fn test_b() {
-        assert_eq!(
-            unsafe { inner::<4, 10000000000000>(TEST.as_bytes()) },
-            875318608908
-        );
     }
 }
