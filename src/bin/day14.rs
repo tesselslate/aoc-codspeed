b7766c91d9f7f63068df5db20ef4eb6db5ff38a0
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-12-15 02:40:06 -0500

    add day 14 bin

---
+++ src/bin/day14.rs
@@ -0,0 +1,26 @@
+use std::time::Instant;
+
+use aoc_codspeed::day14;
+
+const INPUT: &str = include_str!("../../inputs/input14.txt");
+
+fn main() {
+    const N: usize = 1000;
+    let start = Instant::now();
+    for _ in 0..N {
+        std::hint::black_box(day14::part1(INPUT));
+    }
+    println!(
+        "p1: {:.3} usec",
+        Instant::now().duration_since(start).as_nanos() as f64 / 1000.0 / N as f64
+    );
+
+    let start = Instant::now();
+    for _ in 0..N {
+        std::hint::black_box(day14::part2(INPUT));
+    }
+    println!(
+        "p2: {:.3} usec",
+        Instant::now().duration_since(start).as_nanos() as f64 / 1000.0 / N as f64
+    );
+}
