2fc4ab976eb7042eedbfcc52283efb12dd80ffa9
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-12-16 21:38:21 -0500

    no more priority queue
    -89% 17us lmao. only a ~3x improvement under callgrind unfortunately
    
    will need to experiment with the queue size on more inputs, a hard limit
    of ~4000 turns for the *entire* search is a bit tighter than I would
    like

--- src/day16.rs
+++ src/day16.rs
@@ -1,7 +1,5 @@
 #![allow(static_mut_refs)]
 
-use std::collections::BinaryHeap;
-
 const START_OFFSET: isize = 142 * 139 + 1;
 const END_OFFSET: isize = 142 + 139;
 const GRID_SIZE: usize = 142 * 141;
@@ -11,7 +9,9 @@ const D_WEST: isize = -1;
 const D_SOUTH: isize = 142;
 const D_NORTH: isize = -142;
 
-#[derive(PartialEq, Eq)]
+const QSIZE: usize = 4095;
+
+#[derive(Clone, Copy, PartialEq, Eq)]
 struct PqEntry {
     loc: *const u8,
     dir: i32,
@@ -34,7 +34,89 @@ impl PartialOrd for PqEntry {
     }
 }
 
-static mut PQ: BinaryHeap<PqEntry> = BinaryHeap::new();
+struct Queue {
+    step_w: usize,
+    step_r: usize,
+    step: [PqEntry; QSIZE],
+
+    turn_w: usize,
+    turn_r: usize,
+    turn: [PqEntry; QSIZE],
+}
+
+impl Queue {
+    #[inline]
+    pub fn clear(&mut self) {
+        self.step_w = 0;
+        self.step_r = 0;
+
+        self.turn_w = 0;
+        self.turn_r = 0;
+    }
+
+    #[inline]
+    pub fn clear_step(&mut self) {
+        self.step_w = 0;
+        self.step_r = 0;
+    }
+
+    #[inline]
+    pub unsafe fn pop_step(&mut self) -> Option<PqEntry> {
+        if self.step_r == self.step_w {
+            None
+        } else {
+            let entry = *self.step.get_unchecked(self.step_r);
+            self.step_r += 1;
+            Some(entry)
+        }
+    }
+
+    #[inline]
+    pub unsafe fn pop_turn(&mut self, max: usize) -> Option<PqEntry> {
+        if self.turn_r == max {
+            None
+        } else {
+            let entry = *self.turn.get_unchecked(self.turn_r);
+            self.turn_r += 1;
+            Some(entry)
+        }
+    }
+
+    #[inline]
+    pub unsafe fn push_step(&mut self, entry: PqEntry) {
+        *self.step.get_unchecked_mut(self.step_w) = entry;
+        self.step_w += 1;
+    }
+
+    #[inline]
+    pub unsafe fn push_turn(&mut self, entry: PqEntry) {
+        *self.turn.get_unchecked_mut(self.turn_w) = entry;
+        self.turn_w += 1;
+    }
+
+    pub const fn new() -> Self {
+        Self {
+            step_w: 0,
+            step_r: 0,
+            step: [PqEntry {
+                loc: std::ptr::null(),
+                dir: 0,
+                dist: 0,
+            }; QSIZE],
+
+            turn_w: 0,
+            turn_r: 0,
+            turn: [PqEntry {
+                loc: std::ptr::null(),
+                dir: 0,
+                dist: 0,
+            }; QSIZE],
+        }
+    }
+}
+
+static mut PQ: Queue = Queue::new();
+
 static mut DIST: [u32; GRID_SIZE] = [u32::MAX; GRID_SIZE];
 
 unsafe fn inner_p1(input: &[u8]) -> u32 {
@@ -44,11 +126,23 @@ unsafe fn inner_p1(input: &[u8]) -> u32 {
                 let cost = if $entry.dir == $dir as i32 { 2 } else { 1002 };
                 let dist_offset = $entry.loc.offset($dir * 2).sub_ptr($input.as_ptr());
                 if *$DIST.get_unchecked(dist_offset) > $entry.dist + cost {
-                    $PQ.push(PqEntry {
-                        loc: $entry.loc.offset($dir * 2),
-                        dir: $dir as i32,
-                        dist: $entry.dist + cost,
-                    });
+                    if dist_offset == END_OFFSET as usize {
+                        return $entry.dist + cost;
+                    }
+
+                    if $entry.dir == $dir as i32 {
+                        $PQ.push_step(PqEntry {
+                            loc: $entry.loc.offset($dir * 2),
+                            dir: $dir as i32,
+                            dist: $entry.dist + cost,
+                        });
+                    } else {
+                        $PQ.push_turn(PqEntry {
+                            loc: $entry.loc.offset($dir * 2),
+                            dir: $dir as i32,
+                            dist: $entry.dist + cost,
+                        });
+                    }
                     *$DIST.get_unchecked_mut(dist_offset) = $entry.dist + cost;
                 }
             }
@@ -58,24 +152,29 @@ unsafe fn inner_p1(input: &[u8]) -> u32 {
     PQ.clear();
     std::ptr::write_bytes(DIST.as_mut_ptr(), 0xFF, GRID_SIZE);
 
-    PQ.push(PqEntry {
+    PQ.push_step(PqEntry {
         loc: input.as_ptr().offset(START_OFFSET),
         dir: D_EAST as i32,
         dist: 0,
     });
 
-    while let Some(entry) = PQ.pop() {
-        if entry.loc == input.as_ptr().add(END_OFFSET as usize) {
-            return entry.dist;
+    loop {
+        while let Some(entry) = PQ.pop_step() {
+            dijkstra_inner!(entry, D_EAST, DIST, PQ, input);
+            dijkstra_inner!(entry, D_WEST, DIST, PQ, input);
+            dijkstra_inner!(entry, D_SOUTH, DIST, PQ, input);
+            dijkstra_inner!(entry, D_NORTH, DIST, PQ, input);
         }
 
-        dijkstra_inner!(entry, D_EAST, DIST, PQ, input);
-        dijkstra_inner!(entry, D_WEST, DIST, PQ, input);
-        dijkstra_inner!(entry, D_SOUTH, DIST, PQ, input);
-        dijkstra_inner!(entry, D_NORTH, DIST, PQ, input);
+        PQ.clear_step();
+        let end = PQ.turn_w;
+        while let Some(entry) = PQ.pop_turn(end) {
+            dijkstra_inner!(entry, D_EAST, DIST, PQ, input);
+            dijkstra_inner!(entry, D_WEST, DIST, PQ, input);
+            dijkstra_inner!(entry, D_SOUTH, DIST, PQ, input);
+            dijkstra_inner!(entry, D_NORTH, DIST, PQ, input);
+        }
     }
-
-    std::hint::unreachable_unchecked()
 }
 
 pub fn part1(input: &str) -> u32 {
