d77fae938521524e3071a09606f461a1e8becdfa
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-12-18 15:41:24 -0500

    add day 18 bin

---
+++ src/bin/day18.rs
@@ -0,0 +1,26 @@
+use std::time::Instant;
+
+use aoc_codspeed::day18;
+
+const INPUT: &str = include_str!("../../inputs/input18.txt");
+
+fn main() {
+    const N: usize = 10000;
+    let start = Instant::now();
+    for _ in 0..N {
+        std::hint::black_box(day18::part1(std::hint::black_box(INPUT)));
+    }
+    println!(
+        "p1: {:.3} usec",
+        Instant::now().duration_since(start).as_nanos() as f64 / 1000.0 / N as f64
+    );
+
+    let start = Instant::now();
+    for _ in 0..N {
+        std::hint::black_box(day18::part2(std::hint::black_box(INPUT)));
+    }
+    println!(
+        "p2: {:.3} usec",
+        Instant::now().duration_since(start).as_nanos() as f64 / 1000.0 / N as f64
+    );
+}
