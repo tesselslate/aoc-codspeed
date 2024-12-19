7d5c15c263ceae07ac733d48bcd95ad936cc4aac
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-12-19 02:50:24 -0500

    union find
    63us, still sucks but i don't really like optimizing this part so i will
    just get some actual sleep

--- src/day18.rs
+++ src/day18.rs
@@ -70,39 +70,44 @@ unsafe fn inner_p1(input: &[u8]) -> u64 {
 }
 
 unsafe fn inner_p2(input: &[u8]) -> &str {
-    static mut QUEUE: ArrayVec<(u8, u8), QUEUE_SIZE> = ArrayVec::new_const();
-    static mut GRID: [u16; 71 * 71] = [0xFFFF; 71 * 71];
+    static mut UNION: [u16; 71 * 71 + 2] = [0xFFFF; 71 * 71 + 2];
 
-    // clear
-    GRID.iter_mut().for_each(|x| *x = 0xFFFF);
-
-    let mut input = input.as_ptr();
-
-    for _ in 0..1024 {
-        let (x, y): (usize, usize);
-
-        if *input.add(1) == b',' {
-            x = (*input - b'0') as usize;
-            input = input.add(2);
-        } else {
-            x = (*input - b'0') as usize * 10 + (*input.add(1) - b'0') as usize;
-            input = input.add(3);
+    #[inline]
+    unsafe fn merge(union: &mut [u16; 71 * 71 + 2], a: usize, b: usize) {
+        let a = find(union, a);
+        let b = find(union, b);
+        if a != b {
+            *union.get_unchecked_mut(b as usize) = a;
         }
-
-        if *input.add(1) == b'\n' {
-            y = (*input - b'0') as usize;
-            input = input.add(2);
-        } else {
-            y = (*input - b'0') as usize * 10 + (*input.add(1) - b'0') as usize;
-            input = input.add(3);
-        }
-
-        *GRID.get_unchecked_mut(y * 71 + x) = 1;
     }
 
-    let gridptr = GRID.as_mut_ptr();
+    #[inline]
+    unsafe fn find(union: &mut [u16; 71 * 71 + 2], a: usize) -> u16 {
+        if *union.get_unchecked(a) == a as u16 {
+            return a as u16;
+        }
 
-    let mut id = 2;
+        *union.get_unchecked_mut(a) = find(union, *union.get_unchecked(a) as usize);
+        *union.get_unchecked(a)
+    }
+
+    macro_rules! try_offset {
+        ($id: ident, $offset: literal, $UNION: ident) => {{
+            let nid = $id as isize + $offset;
+            if (nid as usize) < 71 * 71 && *$UNION.get_unchecked(nid as usize) != 0xFFFF {
+                merge(&mut $UNION, $id, nid as usize);
+            }
+        }};
+    }
+
+    // clear
+    UNION.iter_mut().for_each(|x| *x = 0xFFFF);
+    UNION[71 * 71] = 71 * 71;
+    UNION[71 * 71 + 1] = 71 * 71 + 1;
+
+    let mut input = input.as_ptr();
+    let mut i = 0;
+
     loop {
         let (x, y): (usize, usize);
         let line = input;
@@ -115,8 +120,6 @@ unsafe fn inner_p2(input: &[u8]) -> &str {
             input = input.add(3);
         }
 
-        // surely it will never be on the last line, since moving the pointer
-        // past is technically ub
         if *input.add(1) == b'\n' {
             y = (*input - b'0') as usize;
             input = input.add(2);
@@ -125,57 +128,45 @@ unsafe fn inner_p2(input: &[u8]) -> &str {
             input = input.add(3);
         }
 
-        *gridptr.add(y * 71 + x) = 1;
+        let id = y * 71 + x;
+        *UNION.get_unchecked_mut(id) = id as u16;
 
-        QUEUE.clear();
-        QUEUE.push_unchecked((x as u8, y as u8));
+        if x > 0 {
+            try_offset!(id, -72, UNION);
+            try_offset!(id, -1, UNION);
+            try_offset!(id, 70, UNION);
+        }
+        if x < 70 {
+            try_offset!(id, -70, UNION);
+            try_offset!(id, 1, UNION);
+            try_offset!(id, 72, UNION);
+        }
+        try_offset!(id, -71, UNION);
+        try_offset!(id, 71, UNION);
 
-        let (mut l, mut r, mut d, mut u) = (false, false, false, false);
+        if x == 0 || y == 70 {
+            merge(&mut UNION, id, 71 * 71);
+        }
+        if x == 70 || y == 0 {
+            merge(&mut UNION, id, 71 * 71 + 1);
+        }
 
-        let mut i = 0;
-        while i < QUEUE.len() {
-            let (x, y) = *QUEUE.get_unchecked(i);
-
-            l |= x == 0;
-            r |= x == 70;
-            d |= y == 70;
-            u |= y == 0;
-
-            for (dx, dy) in [
-                (-1, 0),
-                (1, 0),
-                (0, -1),
-                (0, 1),
-                (-1, -1),
-                (-1, 1),
-                (1, -1),
-                (1, 1),
-            ] {
-                let (nx, ny) = (x as i8 + dx as i8, y as i8 + dy as i8);
-                if nx > 0 && nx <= 70 && ny > 0 && ny <= 70 {
-                    let nptr = gridptr.add(ny as usize * 71 + nx as usize);
-                    if *nptr < id {
-                        *nptr = id;
-                        QUEUE.push_unchecked((nx as u8, ny as u8));
-                    }
-                }
+        if i >= 1024 {
+            if find(&mut UNION, 71 * 71) == find(&mut UNION, 71 * 71 + 1) {
+                return std::str::from_raw_parts(
+                    line,
+                    if *line.add(3) == b'\n' {
+                        3
+                    } else if *line.add(4) == b'\n' {
+                        4
+                    } else {
+                        5
+                    },
+                );
             }
-
-            i += 1;
         }
 
-        if (l || d) && (r || u) {
-            let len = if *line.add(3) == b'\n' {
-                3
-            } else if *line.add(4) == b'\n' {
-                4
-            } else {
-                5
-            };
-            return std::str::from_raw_parts(line, len);
-        }
-
-        id += 1;
+        i += 1;
     }
 }
 
