6044fa0c34a191bed4d890ac4403044bdfd23929
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-12-19 02:34:18 -0500

    even worse

--- src/day18.rs
+++ src/day18.rs
@@ -3,7 +3,6 @@
 use arrayvec::ArrayVec;
 
 const QUEUE_SIZE: usize = 5120;
-const QUEUE_SIZE_2: usize = 1048576;
 const GRID_SIZE: usize = 72 * 73;
 
 unsafe fn inner_p1(input: &[u8]) -> u64 {
@@ -71,25 +70,16 @@ unsafe fn inner_p1(input: &[u8]) -> u64 {
 }
 
 unsafe fn inner_p2(input: &[u8]) -> &str {
-    static mut QUEUE: ArrayVec<(*mut (u16, u16), u32), QUEUE_SIZE_2> = ArrayVec::new_const();
-    static mut GRID: [(u16, u16); GRID_SIZE] = [(0, 0); GRID_SIZE];
-    static mut PT: [*const u8; 3450] = [std::ptr::null(); 3450];
+    static mut QUEUE: ArrayVec<(u8, u8), QUEUE_SIZE> = ArrayVec::new_const();
+    static mut GRID: [u16; 71 * 71] = [0xFFFF; 71 * 71];
 
     // clear
-    GRID.iter_mut().for_each(|x| *x = (0, 0));
-
-    std::ptr::write_bytes(GRID.as_mut_ptr(), 0xFF, 72);
-    std::ptr::write_bytes(GRID.as_mut_ptr().add(72 * 72), 0xFF, 72);
-
-    for i in 1..72 {
-        GRID[i * 72] = (0xFFFF, 0xFFFF);
-    }
+    GRID.iter_mut().for_each(|x| *x = 0xFFFF);
 
     let mut input = input.as_ptr();
 
-    for i in 0..3450 {
+    for _ in 0..1024 {
         let (x, y): (usize, usize);
-        let line = input;
 
         if *input.add(1) == b',' {
             x = (*input - b'0') as usize;
@@ -101,66 +91,92 @@ unsafe fn inner_p2(input: &[u8]) -> &str {
 
         if *input.add(1) == b'\n' {
             y = (*input - b'0') as usize;
-            if i != 3449 {
-                input = input.add(2);
-            }
+            input = input.add(2);
         } else {
             y = (*input - b'0') as usize * 10 + (*input.add(1) - b'0') as usize;
-            if i != 3449 {
-                input = input.add(3);
-            }
+            input = input.add(3);
         }
 
-        *GRID.get_unchecked_mut(y * 72 + x + 73) = (i + 1, 0);
-        PT[i as usize] = line;
+        *GRID.get_unchecked_mut(y * 71 + x) = 1;
     }
 
     let gridptr = GRID.as_mut_ptr();
 
-    QUEUE.clear();
-    QUEUE.push((gridptr.add(73), 3449));
+    let mut id = 2;
+    loop {
+        let (x, y): (usize, usize);
+        let line = input;
 
-    let mut i = 0;
-    while i < QUEUE.len() {
-        let (loc, mut time) = *QUEUE.get_unchecked(i);
-
-        #[allow(non_snake_case)]
-        let (O, M) = *loc;
-        debug_assert!(O != 0xFFFF && M != 0xFFFF);
-
-        if O > 0 {
-            time = u32::min(time, O as u32);
+        if *input.add(1) == b',' {
+            x = (*input - b'0') as usize;
+            input = input.add(2);
+        } else {
+            x = (*input - b'0') as usize * 10 + (*input.add(1) - b'0') as usize;
+            input = input.add(3);
         }
-        if M >= time as u16 {
-            i += 1;
-            continue;
-        }
-        (*loc).1 = time as u16;
 
-        for offset in [1, -1, 72, -72] {
-            let nloc = loc.offset(offset);
-            if *nloc == (0xFFFF, 0xFFFF) {
-                continue;
+        // surely it will never be on the last line, since moving the pointer
+        // past is technically ub
+        if *input.add(1) == b'\n' {
+            y = (*input - b'0') as usize;
+            input = input.add(2);
+        } else {
+            y = (*input - b'0') as usize * 10 + (*input.add(1) - b'0') as usize;
+            input = input.add(3);
+        }
+
+        *gridptr.add(y * 71 + x) = 1;
+
+        QUEUE.clear();
+        QUEUE.push_unchecked((x as u8, y as u8));
+
+        let (mut l, mut r, mut d, mut u) = (false, false, false, false);
+
+        let mut i = 0;
+        while i < QUEUE.len() {
+            let (x, y) = *QUEUE.get_unchecked(i);
+
+            l |= x == 0;
+            r |= x == 70;
+            d |= y == 70;
+            u |= y == 0;
+
+            for (dx, dy) in [
+                (-1, 0),
+                (1, 0),
+                (0, -1),
+                (0, 1),
+                (-1, -1),
+                (-1, 1),
+                (1, -1),
+                (1, 1),
+            ] {
+                let (nx, ny) = (x as i8 + dx as i8, y as i8 + dy as i8);
+                if nx > 0 && nx <= 70 && ny > 0 && ny <= 70 {
+                    let nptr = gridptr.add(ny as usize * 71 + nx as usize);
+                    if *nptr < id {
+                        *nptr = id;
+                        QUEUE.push_unchecked((nx as u8, ny as u8));
+                    }
+                }
             }
 
-            QUEUE.push((nloc, time));
+            i += 1;
         }
 
-        i += 1;
+        if (l || d) && (r || u) {
+            let len = if *line.add(3) == b'\n' {
+                3
+            } else if *line.add(4) == b'\n' {
+                4
+            } else {
+                5
+            };
+            return std::str::from_raw_parts(line, len);
+        }
+
+        id += 1;
     }
-
-    let time = GRID[72 * 72 - 1].1 - 1;
-    let line = *PT.get_unchecked(time as usize);
-
-    let len = if *line.add(3) == b'\n' {
-        3
-    } else if *line.add(4) == b'\n' {
-        4
-    } else {
-        5
-    };
-
-    std::str::from_raw_parts(line, len)
 }
 
 pub fn part1(input: &str) -> u64 {
