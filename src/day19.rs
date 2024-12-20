2bfb503ef8243e0aa3a3ec2499394dbd92fc8a50
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-12-19 20:37:48 -0500

    day 19 part 1 trie
    ~126us, this took way too long because i was handling newlines wrong

--- src/day19.rs
+++ src/day19.rs
@@ -1,7 +1,5 @@
 #![allow(static_mut_refs)]
 
-use arrayvec::ArrayVec;
-
 const NUM_PATTERNS: usize = 400;
 
 #[derive(Clone, Copy, PartialEq, Eq)]
@@ -10,21 +8,61 @@ struct UnsafeSlice {
     len: usize,
 }
 
-impl UnsafeSlice {
+#[inline]
+#[target_feature(enable = "popcnt")]
+unsafe fn phf(x: u8) -> usize {
+    (x.count_ones() as u8 - (x & 0b10) - 1) as usize
+}
+
+struct Trie {
+    free: usize,
+    data: [isize; 16384],
+}
+
+impl Trie {
+    pub const fn new() -> Self {
+        Self {
+            free: 6,
+            data: [-1; 16384],
+        }
+    }
+
     #[inline]
-    pub unsafe fn matches(&self, other: *const u8) -> *const u8 {
-        for i in 0..self.len {
-            if *self.data.add(i) != *other.add(i) {
-                return std::ptr::null();
+    pub fn clear(&mut self) {
+        self.free = 6;
+        self.data[0..6].iter_mut().for_each(|x| *x = -1);
+    }
+
+    #[inline]
+    pub unsafe fn insert(&mut self, pattern: UnsafeSlice) {
+        let mut node_index = 0;
+
+        for i in 0..pattern.len {
+            let char = *pattern.data.add(i);
+            let idx = node_index + phf(char);
+            let next = *self.data.get_unchecked(idx);
+
+            if next > 0 {
+                node_index = next as usize;
+            } else {
+                *self.data.get_unchecked_mut(idx) = self.free as isize;
+                node_index = self.free;
+
+                self.data
+                    .get_unchecked_mut(self.free..self.free + 6)
+                    .iter_mut()
+                    .for_each(|x| *x = -1);
+
+                self.free += 6;
             }
         }
 
-        other.add(self.len)
+        *self.data.get_unchecked_mut(node_index + 5) = 0;
     }
 }
 
 unsafe fn inner_p1(input: &[u8]) -> u32 {
-    static mut TOWELS: ArrayVec<UnsafeSlice, 16384> = ArrayVec::new_const();
+    static mut TOWELS: Trie = Trie::new();
 
     TOWELS.clear();
 
@@ -35,7 +73,7 @@ unsafe fn inner_p1(input: &[u8]) -> u32 {
         while *input != b',' {
             input = input.add(1);
             if *input == b'\n' {
-                TOWELS.push_unchecked(UnsafeSlice {
+                TOWELS.insert(UnsafeSlice {
                     data: start,
                     len: input.sub_ptr(start),
                 });
@@ -43,35 +81,56 @@ unsafe fn inner_p1(input: &[u8]) -> u32 {
             }
         }
 
-        TOWELS.push_unchecked(UnsafeSlice {
+        TOWELS.insert(UnsafeSlice {
             data: start,
             len: input.sub_ptr(start),
         });
         input = input.add(2);
     }
 
-    unsafe fn dfs(cache: *mut u64, start: *const u8, target: *const u8) -> *const u8 {
-        if *target == b'\n' {
-            return target;
-        }
+    debug_assert!(TOWELS.data[5] == -1);
+    debug_assert!(phf(b'b') == 0);
+    debug_assert!(phf(b'r') == 1);
+    debug_assert!(phf(b'g') == 2);
+    debug_assert!(phf(b'w') == 3);
+    debug_assert!(phf(b'u') == 4);
 
-        let cache_idx = target.sub_ptr(start);
+    unsafe fn dfs(trie: &Trie, cache: *mut u64, start: *const u8, mut offset: usize) -> usize {
+        let cache_idx = offset;
         if (*cache & (1 << cache_idx)) != 0 {
-            return std::ptr::null();
+            return 0;
         }
 
-        for towel in &TOWELS {
-            let next = towel.matches(target);
-            if !next.is_null() {
-                let ptr = dfs(cache, start, next);
-                if !ptr.is_null() {
-                    return ptr;
+        let mut trie_node = 0;
+        loop {
+            let char = *start.add(offset);
+            if char == b'\n' {
+                if *trie.data.get_unchecked(trie_node + 5) == 0 {
+                    return offset;
+                } else {
+                    return 0;
                 }
             }
-        }
+            debug_assert!(b"bwrug".contains(&char));
 
-        *cache |= 1 << cache_idx;
-        std::ptr::null()
+            let idx = trie_node + phf(char);
+            let next = *trie.data.get_unchecked(idx);
+
+            if next < 0 {
+                *cache |= 1 << cache_idx;
+                return 0;
+            }
+
+            if *trie.data.get_unchecked(next as usize + 5) == 0 {
+                let ret = dfs(trie, cache, start, offset + 1);
+                if ret > 0 {
+                    return ret;
+                }
+            }
+
+            trie_node = next as usize;
+            offset += 1;
+        }
     }
 
     let mut valid = 0;
@@ -80,13 +139,14 @@ unsafe fn inner_p1(input: &[u8]) -> u32 {
     for _ in 0..NUM_PATTERNS {
         debug_assert!(*input != b'\n');
 
-        let start = input;
         let mut cache = 0u64;
-        let next = dfs(std::ptr::from_mut(&mut cache), start, start);
+        let next = dfs(&TOWELS, std::ptr::from_mut(&mut cache), input, 0);
 
-        if !next.is_null() {
+        if next > 0 {
+            debug_assert!(*input.add(next) == b'\n');
+            input = input.add(next + 1);
+
             valid += 1;
-            input = next.add(1);
         } else {
             while *input != b'\n' {
                 input = input.add(1);
