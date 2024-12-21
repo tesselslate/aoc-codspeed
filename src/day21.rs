51d12081f8ac742d71ba343704b9a6056e942666
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-12-21 17:47:04 -0500

    day 21 impl
    Lol

--- src/day21.rs
+++ src/day21.rs
@@ -1,12 +1,52 @@
-unsafe fn inner_p1(input: &[u8]) -> u32 {
-    0
+use std::arch::x86_64::_pext_u32;
+
+const LUT_RAW: &[u8] = include_bytes!("../lut/day21.bin");
+
+fn lut_lookup_p1(code: usize) -> u64 {
+    let offset = code * 8;
+
+    u64::from_ne_bytes(unsafe {
+        LUT_RAW
+            .get_unchecked(offset..offset + 8)
+            .try_into()
+            .unwrap_unchecked()
+    })
+}
+
+fn lut_lookup_p2(code: usize) -> u64 {
+    let offset = code * 8;
+
+    u64::from_ne_bytes(unsafe {
+        LUT_RAW
+            .get_unchecked(offset + 32768..offset + 32768 + 8)
+            .try_into()
+            .unwrap_unchecked()
+    })
+}
+
+#[inline]
+unsafe fn pext_bits(code: &[u8]) -> usize {
+    let u32 = u32::from_ne_bytes(code.get_unchecked(0..4).try_into().unwrap_unchecked());
+    unsafe { _pext_u32(u32, 0x000F0F0F) as usize }
+}
+
+unsafe fn inner_p1(input: &[u8]) -> u64 {
+    lut_lookup_p1(pext_bits(input))
+        + lut_lookup_p1(pext_bits(input.get_unchecked(5..)))
+        + lut_lookup_p1(pext_bits(input.get_unchecked(10..)))
+        + lut_lookup_p1(pext_bits(input.get_unchecked(15..)))
+        + lut_lookup_p1(pext_bits(input.get_unchecked(20..)))
 }
 
 unsafe fn inner_p2(input: &[u8]) -> u64 {
-    0
+    lut_lookup_p2(pext_bits(input))
+        + lut_lookup_p2(pext_bits(input.get_unchecked(5..)))
+        + lut_lookup_p2(pext_bits(input.get_unchecked(10..)))
+        + lut_lookup_p2(pext_bits(input.get_unchecked(15..)))
+        + lut_lookup_p2(pext_bits(input.get_unchecked(20..)))
 }
 
-pub fn part1(input: &str) -> u32 {
+pub fn part1(input: &str) -> u64 {
     unsafe { inner_p1(input.as_bytes()) }
 }
 
