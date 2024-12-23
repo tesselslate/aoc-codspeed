f00859103690ad36dd48fb3007ceb867942e39a6
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-12-22 22:03:29 -0500

    part 2 initial impl
    12.2ms

--- src/day22.rs
+++ src/day22.rs
@@ -1,10 +1,24 @@
+#![allow(static_mut_refs)]
+
+use fastdiv::{FastDiv, PrecomputedDivU32};
+
+const SEQ_COUNT: usize = 19 * 19 * 19 * 19;
+
+const MAGIC2000: [[u32; 256]; 3] = create_magic::<2000>();
+
 // shoutout incompleteusern
-const MAGIC_8: [[u32; 256]; 3] = const {
+const fn create_magic<const ITERATIONS: usize>() -> [[u32; 256]; 3] {
     let mut bits = [0u32; 24];
 
     let mut i = 0;
     while i < 24 {
-        bits[i] = hash_2k(1 << i);
+        let mut j = 0;
+        let mut x = 1 << i;
+        while j < ITERATIONS {
+            x = hash(x);
+            j += 1;
+        }
+        bits[i] = x;
         i += 1;
     }
 
@@ -44,7 +58,7 @@ const MAGIC_8: [[u32; 256]; 3] = const {
     }
 
     lut
-};
+}
 
 #[inline(always)]
 const fn hash(mut x: u32) -> u32 {
@@ -53,21 +67,12 @@ const fn hash(mut x: u32) -> u32 {
     (x ^ (x << 11)) & 0xFFFFFF
 }
 
-const fn hash_2k(mut x: u32) -> u32 {
-    let mut i = 0;
-    while i < 2000 {
-        x = hash(x);
-        i += 1;
-    }
-    x
-}
-
 #[inline(always)]
 unsafe fn skip_2k(x: u32) -> u32 {
     let mut s = 0;
 
     for i in 0..3 {
-        s ^= MAGIC_8
+        s ^= MAGIC2000
             .get_unchecked(i)
             .get_unchecked(((x >> (i * 8)) & 0xFF) as usize);
     }
@@ -97,15 +102,64 @@ unsafe fn inner_p1(input: &[u8]) -> u64 {
     }
 }
 
-unsafe fn inner_p2(input: &[u8]) -> u32 {
-    0
+unsafe fn inner_p2(input: &[u8]) -> i16 {
+    let d10: PrecomputedDivU32 = 10u32.precompute_div();
+    let d130321: PrecomputedDivU32 = 130321u32.precompute_div();
+
+    static mut VALUE: [i16; SEQ_COUNT] = [0; SEQ_COUNT];
+    static mut SEEN: [u16; SEQ_COUNT] = [0; SEQ_COUNT];
+
+    VALUE.iter_mut().for_each(|x| *x = 0);
+    SEEN.iter_mut().for_each(|x| *x = 0);
+
+    let mut best = 0;
+
+    let input_end = input.as_ptr_range().end.sub(1);
+    let mut input = input.as_ptr();
+
+    let mut secret_id = 1;
+    loop {
+        let mut secret = 0;
+        while *input != b'\n' {
+            secret = secret * 10 + (*input - b'0') as u32;
+            input = input.add(1);
+        }
+
+        let mut seq_id = 0u32;
+        for _ in 0..4 {
+            let next = hash(secret);
+            seq_id = seq_id * 19 + (9 + next.fast_mod(d10, 10) - secret.fast_mod(d10, 10));
+
+            secret = next;
+        }
+
+        for _ in 0..1996 {
+            if *SEEN.get_unchecked(seq_id as usize) < secret_id {
+                *SEEN.get_unchecked_mut(seq_id as usize) = secret_id;
+                *VALUE.get_unchecked_mut(seq_id as usize) += secret.fast_mod(d10, 10) as i16;
+                best = i16::max(best, *VALUE.get_unchecked(seq_id as usize));
+            }
+
+            let next = hash(secret);
+            seq_id = (seq_id * 19).fast_mod(d130321, 130321)
+                + (9 + next.fast_mod(d10, 10) - secret.fast_mod(d10, 10));
+            secret = next;
+        }
+
+        if input == input_end {
+            return best;
+        }
+
+        input = input.add(1);
+        secret_id += 1;
+    }
 }
 
 pub fn part1(input: &str) -> u64 {
     unsafe { inner_p1(input.as_bytes()) }
 }
 
-pub fn part2(input: &str) -> u32 {
+pub fn part2(input: &str) -> i16 {
     unsafe { inner_p2(input.as_bytes()) }
 }
 
