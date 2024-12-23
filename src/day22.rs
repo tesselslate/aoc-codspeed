c577b657f1fcc2add2a90c0fb23b40b8cc5d0bea
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-12-22 19:04:08 -0500

    use fast PRNG jumpahead
    ~9us uncached ~7us cached, shoutout incompleteusern for the idea to
    abuse the associativity of the PRNG to speed up jumping ahead

--- src/day22.rs
+++ src/day22.rs
@@ -1,5 +1,88 @@
+// shoutout incompleteusern
+const MAGIC_4: [[u32; 16]; 6] = const {
+    let mut bits = [0u32; 24];
+
+    let mut i = 0;
+    while i < 24 {
+        bits[i] = hash_2k(1 << i);
+        i += 1;
+    }
+
+    let mut lut = [[0; 16]; 6];
+
+    let mut i = 0;
+    while i < 6 {
+        let mut j = 0;
+        while j < 16 {
+            if (j & 1) != 0 {
+                lut[i][j] ^= bits[i * 4];
+            }
+            if (j & 2) != 0 {
+                lut[i][j] ^= bits[i * 4 + 1];
+            }
+            if (j & 4) != 0 {
+                lut[i][j] ^= bits[i * 4 + 2];
+            }
+            if (j & 8) != 0 {
+                lut[i][j] ^= bits[i * 4 + 3];
+            }
+            j += 1;
+        }
+        i += 1;
+    }
+
+    lut
+};
+
+#[inline(always)]
+const fn hash(mut x: u32) -> u32 {
+    x = (x ^ (x << 6)) & 0xFFFFFF;
+    x = x ^ (x >> 5);
+    (x ^ (x << 11)) & 0xFFFFFF
+}
+
+const fn hash_2k(mut x: u32) -> u32 {
+    let mut i = 0;
+    while i < 2000 {
+        x = hash(x);
+        i += 1;
+    }
+    x
+}
+
+#[inline(always)]
+unsafe fn skip_2k(x: u32) -> u32 {
+    let mut s = 0;
+
+    for i in 0..6 {
+        s ^= MAGIC_4
+            .get_unchecked(i)
+            .get_unchecked(((x >> (i * 4)) & 0xF) as usize);
+    }
+
+    s
+}
+
 unsafe fn inner_p1(input: &[u8]) -> u64 {
-    0
+    let mut sum = 0;
+
+    let input_end = input.as_ptr_range().end.sub(1);
+    let mut input = input.as_ptr();
+
+    loop {
+        let mut acc = 0;
+        while *input != b'\n' {
+            acc = acc * 10 + (*input - b'0') as u32;
+            input = input.add(1);
+        }
+
+        sum += skip_2k(acc) as u64;
+        if input == input_end {
+            return sum;
+        }
+
+        input = input.add(1);
+    }
 }
 
 unsafe fn inner_p2(input: &[u8]) -> u32 {
